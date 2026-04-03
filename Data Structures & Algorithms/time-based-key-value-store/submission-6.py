c84c964dee4078b8036b916231daac74e61c0690
omarina <98719820+ovidium3@users.noreply.github.com>
2026-04-03 16:30:27 -0400

    Add: time-based-key-value-store - submission-6

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-6.py
@@ -0,0 +1,30 @@
+class TimeMap:
+    # we can try to do log n retrieval for get, where
+    # n == the length of the duplicate entries for
+    # a specific key
+    # chance that we dont find a good val
+    # and thus need rightmost value to return
+    # assuming r did not underflow to -1
+    # in which case we did not find res and return ""
+
+    def __init__(self):
+        self.m = defaultdict(list) # store val, timestamp pairs
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.m[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        vals = self.m[key]
+        if len(vals) == 0:
+            return ""
+        l, r = 0, len(vals) - 1
+        while l <= r:
+            m = l + (r - l) // 2 # to avoid possibel overflow
+            val, time = vals[m]
+            if time <= timestamp:
+                l = m + 1
+            elif time > timestamp:
+                r = m - 1
+            else: # time == timestamp
+                return val
+        return vals[r][0] if r >= 0 else ""
